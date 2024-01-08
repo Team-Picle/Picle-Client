4d39bd0bd9efa6f4ca93712760e6ac1a1dddd99e
안가은 <gaeun1021@sookmyung.ac.kr>
2024-01-08 17:51:00 +0900

    #6 [FEAT] 하단 모달 위젯 뷰 구현 완료

---
+++ picle/lib/widgets/add_modal_widget.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:picle/models/default_button_model.dart';
+import 'package:image_picker/image_picker.dart';
+
+void addBottomModal({
+  required BuildContext context,
+  required String title,
+  required String content,
+  required String buttonText,
+  required bool needImg,
+}) {
+  showModalBottomSheet(
+    context: context,
+    builder: (BuildContext context) {
+      Widget renderEmpty() {
+        return Container();
+      }
+
+      Widget renderAddImg() {
+        return GestureDetector(
+          onTap: () async {
+            var picker = ImagePicker();
+            var image = await picker.pickImage(source: ImageSource.gallery);
+          },
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SvgPicture.asset('lib/images/picture_icon.svg'),
+              const SizedBox(width: 12),
+              const Text(
+                '이미지 등록',
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      return Container(
+        height: 345,
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SvgPicture.asset('lib/images/home_indicator.svg'),
+            const SizedBox(height: 20),
+            Text(
+              title,
+              style: const TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 25,
+              ),
+            ),
+            const SizedBox(height: 27),
+            Text(
+              content,
+              style: const TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 15,
+              ),
+            ),
+            const SizedBox(height: 27),
+            needImg ? renderAddImg() : renderEmpty(),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              style: buttonStyle,
+              onPressed: null,
+              child: Text(buttonText),
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
--- picle/lib/widgets/button.dart
+++ picle/lib/widgets/button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:picle/widgets/add_modal_widget.dart';
 
 class Button extends StatelessWidget {
   final String text;
@@ -11,7 +12,14 @@ class Button extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return OutlinedButton(
-      onPressed: () {},
+      onPressed: () => addBottomModal(
+        context: context,
+        title: '루틴을 입력하세요.',
+        content: '루틴을 인증할 사진을 등록하고'
+            '\n매일 같은 구도로 사진을 촬영해서 인증해보세요!',
+        buttonText: '루틴 등록하기',
+        needImg: true,
+      ),
       style: ButtonStyle(
         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
           RoundedRectangleBorder(
