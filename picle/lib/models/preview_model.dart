776e2b947ab04a560332539ce8a7b4bb46efc72f
haesa <xdhaesa@gmail.com>
2024-03-08 18:13:42 +0900

    #35 [FIX] 루틴 및 미리보기 뷰 오류 해결

---
+++ picle/lib/models/preview_model.dart
@@ -0,0 +1,25 @@
+class Preview {
+  final int userId;
+  final int routineId;
+  final String content;
+  final String time;
+  final String startRepeatDate;
+  final List<dynamic> repeatDays;
+
+  Preview({
+    required this.userId,
+    required this.routineId,
+    required this.content,
+    required this.time,
+    required this.startRepeatDate,
+    required this.repeatDays,
+  });
+
+  Preview.fromJson(Map<String, dynamic> json)
+      : userId = json['userId'],
+        routineId = json['routineId'],
+        content = json['content'],
+        time = json['time'],
+        startRepeatDate = json['startRepeatDate'],
+        repeatDays = json['repeatDays'];
+}
