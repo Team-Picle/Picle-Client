090ad7dae4722c3e66c332ca83c772f8fe2c267c
haesa <xdhaesa@gmail.com>
2024-03-28 14:11:41 +0900

    [FIX] 마이피드 오류 수정

--- picle/lib/widgets/list/my_feed_item.dart
+++ picle/lib/widgets/list/my_feed_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class MyFeedItem extends StatefulWidget {
   final String imageUrl;
+
   final String date;
   final String profileImage;
   final int likeCount;
@@ -19,74 +20,70 @@ class MyFeedItem extends StatefulWidget {
 }
 
 class _MyFeedItemState extends State<MyFeedItem> {
-  bool isLiked = false;
-
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Column(
-        children: [
-          Card(
-            color: Colors.white,
-            // margin: const EdgeInsets.all(13.0),
-            child: Stack(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Image.network(
-                      widget.imageUrl,
-                      fit: BoxFit.cover,
-                      // width: 150,
-                      height: 362,
-                    ),
-                  ],
-                ),
-                Positioned(
-                  left: 8.0,
-                  bottom: 3.0,
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Stack(
-                      children: [
-                        Icon(
-                          Icons.favorite,
-                          color: Colors.red,
-                        ),
-                      ],
-                    ),
+    return Column(
+      children: [
+        Card(
+          color: Colors.white,
+          // margin: const EdgeInsets.all(13.0),
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Image.network(
+                    widget.imageUrl,
+                    fit: BoxFit.cover,
+                    // width: 150,
+                    height: 362,
+                  ),
+                ],
+              ),
+              Positioned(
+                left: 8.0,
+                bottom: 3.0,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Stack(
+                    children: [
+                      Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                      ),
+                    ],
                   ),
                 ),
-                Positioned(
-                  left: 50.0,
-                  bottom: 16.0,
-                  child: Text(
-                    '${widget.likeCount}',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 17,
-                    ),
+              ),
+              Positioned(
+                left: 50.0,
+                bottom: 16.0,
+                child: Text(
+                  '${widget.likeCount}',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
                   ),
                 ),
-                Positioned(
-                  right: 15.0,
-                  bottom: 20.0,
-                  child: Text(
-                    'Date: ${widget.date}',
-                    style: const TextStyle(
-                      fontSize: 12.0,
-                      color: Colors.white,
-                    ),
+              ),
+              Positioned(
+                right: 15.0,
+                bottom: 20.0,
+                child: Text(
+                  'Date: ${widget.date}',
+                  style: const TextStyle(
+                    fontSize: 12.0,
+                    color: Colors.white,
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-          const SizedBox(
-            height: 30,
-          ),
-        ],
-      ),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+      ],
     );
   }
 }
